cc9edcd09156e4168b54faf6dd2c1c19b246ec67
Crowdbotics <engineering@crowdbotics.com>
2023-12-25 15:06:45 +0000

    add component Push Notifications

--- modules/firebase-push-notifications/api.js
+++ modules/firebase-push-notifications/api.js
@@ -1,7 +1,9 @@
 import { getGlobalOptions } from "@options";
-const global = getGlobalOptions(); // Change your BASE_URL in `options/options.js` to edit this value
 
+const global = getGlobalOptions();
+// Change your BASE_URL in `options/options.js` to edit this value
 const BASE_URL = global.url;
+
 export const registerDeviceInfoAPI = async (data, authToken) => {
   const response = await fetch(`${BASE_URL}/modules/firebase-push-notifications/user_fcm_device_add/`, {
     headers: {
@@ -11,16 +13,21 @@ export const registerDeviceInfoAPI = async (data, authToken) => {
     method: "POST",
     body: JSON.stringify(data)
   });
+
   const res = await response.json();
   return res;
 };
-export const fetchNotifications = async authToken => {
-  const response = await fetch(`${BASE_URL}/modules/firebase-push-notifications/notification/`, {
-    method: "GET",
-    headers: {
-      Authorization: "Token " + authToken
+
+export const fetchNotifications = async (authToken) => {
+  const response = await fetch(
+    `${BASE_URL}/modules/firebase-push-notifications/notification/`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Token " + authToken
+      }
     }
-  });
+  );
   const res = await response.json();
   return res;
 };
