004fce8d27c65f3e9010f1a9b8bdf7f65f66b016
Dmitrii Alferev <dmitry@alferev.com>
2023-12-25 16:40:45 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- backend/modules/firebase_push_notifications/firebase-push-notifications/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.contrib.admin.helpers import ActionForm
-
-
-class CustomAdminNotificationForm(ActionForm):
-    title = forms.CharField(label="Title:", required=True)
-    message = forms.CharField(label="Message:", widget=forms.Textarea, required=True)
